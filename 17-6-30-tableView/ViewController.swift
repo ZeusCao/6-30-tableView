accd251e7e95c17a1842da8add844c333bf18646
曹檑 <1090491594@qq.com>
2017-07-14 13:59:20 +0800

    tableView

--- 17-6-30-tableView/ViewController.swift
+++ 17-6-30-tableView/ViewController.swift
@@ -8,18 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        setUpUI()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setUpUI() {
+        
+        // 1.创建表格
+        let tv = UITableView(frame: view.bounds, style: .plain)
+        
+        // 2.添加到视图
+        view.addSubview(tv)
+        
+        // 3.注册可重用cell（UITableViewcell class）
+        tv.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+        
+        // 4. 设置数据源
+        tv.dataSource = self
+        
     }
-
+    
+    // MARK --- UITableViewDataSource 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        
+        // 提示 textLabel是可选的
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
+    
+    
 
 }
 
